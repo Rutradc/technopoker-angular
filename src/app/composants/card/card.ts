cf58f70830a4460543953f9043d81d4aa80b40f3
Rutradc <arthurdc@live.be>
2026-06-23 15:44:33 +0200

    added facedown and other design for card component

--- src/app/composants/card/card.ts
+++ src/app/composants/card/card.ts
@@ -9,62 +9,46 @@ import { CardModel } from '../../models/cardModel';
 })
 export class Card {
   @Input() card: CardModel | undefined;
-  @Input() value : string = '';
-  @Input() suit : string = '';
   @Input() size : string = 'm';
+  @Input() isFaceDown : boolean = false;
+  @Input() faceDownStyle : string = 'classic';
   suitIcon: string = '';
   valueShowed: string = '';
   @HostBinding('style.--color') color = '#d40000';
   @HostBinding('style.--size') sizeClass = 'm';
 
   ngOnInit(): void {
-    if (!this.card) {
-      this.card = new CardModel(this.value, this.suit);
-    }
+    this.sizeClass = this.size;
 
-    switch (this.card.suit){
+    if (!this.isFaceDown) {
+      this.buildCard();
+    }
+  }
+
+  private buildCard() {
+    switch (this.card!.suit) {
       case 'hearts':
-        this.suitIcon = '♥'
+        this.suitIcon = '♥';
         break;
       case 'diamonds':
-        this.suitIcon = '♦'
+        this.suitIcon = '♦';
         break;
       case 'clubs':
-        this.suitIcon = '♣'
-        this.color = '#000000'
+        this.suitIcon = '♣';
+        this.color = '#000000';
         break;
       case 'spades':
-        this.suitIcon = '♠'
-        this.color = '#000000'
-    }
-    
-    switch (this.card.value){
-      case 'king':
-        this.valueShowed = 'K'
+        this.suitIcon = '♠';
+        this.color = '#000000';
         break;
-      case 'queen':
-        this.valueShowed = 'Q'
-        break;
-      case 'jack':
-        this.valueShowed = 'J'
-        break;
-      case 'ace':
-        this.valueShowed = 'A'
-        break;
-      default:
-        this.valueShowed = this.card.value
     }
 
-    switch (this.size){
-      case 's':
-        this.sizeClass = 's'
-        break;
-      case 'm':
-        this.sizeClass = 'm'
-        break;
-      case 'l':
-        this.sizeClass = 'l'
-        break;
+    switch (this.card!.value) {
+      case 'king': this.valueShowed = 'K'; break;
+      case 'queen': this.valueShowed = 'Q'; break;
+      case 'jack': this.valueShowed = 'J'; break;
+      case 'ace': this.valueShowed = 'A'; break;
+      default: this.valueShowed = this.card!.value;
     }
   }
 }
