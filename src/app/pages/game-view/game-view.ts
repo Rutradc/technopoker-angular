ed8569bcd0904e6814e66db069b164b90088ce98
Rutradc <arthurdc@live.be>
2026-06-25 12:00:32 +0200

    feat(animations) added messages that pop next to player name when player action

--- src/app/pages/game-view/game-view.ts
+++ src/app/pages/game-view/game-view.ts
@@ -20,6 +20,11 @@ export class GameView {
   raiseForm: FormGroup;
   raiseAmount = 10;
 
+  playerMessages: Record<
+    string,
+    { text: string; type: 'fold' | 'call' | 'raise' | 'allin' } | null
+  > = {};
+
   pot$ = computed(() => {
     const table = this.table$();
     return table ? table.pot : 0;
@@ -67,6 +72,10 @@ export class GameView {
       await this.tableService.joinTable(Number(this.route.snapshot.paramMap.get('id')));
     }
     this.showTurnNotification();
+    this.showPlayerMessage(this.tableService.username$(), "ALL IN", 'allin');
+    // this.showPlayerMessage(this.tableService.username$(), "Folded", 'fold');
+    // this.showPlayerMessage(this.tableService.username$(), "Call", 'call');
+    // this.showPlayerMessage(this.tableService.username$(), "Raise", 'raise');
   }
 
   fold() {
@@ -90,5 +99,14 @@ export class GameView {
       this.showTurnMessage = false;
       this.cdr.detectChanges();
     }, 1900);
+  }
+
+  showPlayerMessage(playerName: string, message: string, message_type: 'fold' | 'call' | 'raise' | 'allin') {
+    this.playerMessages[playerName] = { text: message, type: message_type};
+
+    setTimeout(() => {
+      this.playerMessages[playerName] = null;
+      this.cdr.detectChanges();
+    }, 1500);
   }
 }
